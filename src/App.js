8e6966b524649d202fa8ac1b34399e8fa377129e
ziihh <zainb@stud.ntnu.no>
2020-02-28 18:27:36 +0100

    added UI layout for notes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React from "react";
 import StickyNotesHome from "./components/Home/home.jsx";
 
 export default function App() {
-  return ( <div>
-    <StickyNotesHome />
+  return (
+  	<div>
+    	<StickyNotesHome />
     </div>
   );
 }
--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import classes from "./home.module.scss";
 
 export default function StickyNotesHome() {
 	return (
-		<div>
-			<button>Create note</button>
+		<div className={classes.st}>
+
+			<div className={classes.Sticky}>
+				<div className={classes.Header}>
+					<label for="title" />
+	  				<input type="text"  name="title" placeholder="title" className={classes.title} />
+	  			</div>
+	  			<div className="NoteArea">
+	  				<textarea className={classes.content} rows="4" cols="50">
+	  					hellow
+					</textarea>
+	  			</div>
+			</div>
+
+
 		</div>
 	);
 }
