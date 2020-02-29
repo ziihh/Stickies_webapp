dc93ddd5229d3d43bed3c8a6138a8760a3a0b153
ziihh <zainb@stud.ntnu.no>
2020-02-29 15:00:38 +0100

    Added background image, fixed the Notes placing layout, data is presistant in notes as long browser is open using localstorage to do so and fixed the overall layout

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React from "react";
+import $ from "jquery";
 
 import classes from "./home.module.scss";
 import StickyNote from "../StickyNote/StickyNote.jsx";
@@ -9,6 +10,7 @@ import plus from "../../media/SVG/plus.svg";
 import file from "../../media/SVG/file.svg";
 import trash from "../../media/SVG/trash.svg";
 import draw from "../../media/SVG/draw.svg";
+import arrow from "../../media/SVG/arrow.svg";
 class Home extends React.Component {
 
 	constructor(props) {
@@ -19,14 +21,16 @@ class Home extends React.Component {
 		this.onChangeColorSelection = this.onChangeColorSelection.bind(this);
 		this.onPageUnload = this.onPageUnload.bind(this);
 		this.getInitState = this.getInitState.bind(this);
+		this.onHashChange = this.onHashChange.bind(this);
 
-		this.state = this.getInitState();
-
-		window.onbeforeunload = this.onPageUnload();
-
+	    this.state = this.getInitState();
 
 	}
-
+	componentDidMount() {
+		this.state.selectedColor = window.location.hash.split('#')[1];
+      	window.addEventListener("beforeunload", this.onPageUnload);
+      	window.addEventListener("hashchange", this.onHashChange);
+    }
 	addNote() {
 		this.setState({
 			nrOfNotes: this.state.nrOfNotes + 1
@@ -36,7 +40,8 @@ class Home extends React.Component {
 	deleteAllNotes(){
 		this.setState({
 		  nrOfNotes: 0,
-		  notes: [],
+		  notesData: [],
+		  renderNotes: [],
 		  selectedColor: "YELLOWNOTE"
 		});
 	}
@@ -44,7 +49,8 @@ class Home extends React.Component {
 	getInitState() {
 		var selectColor = "YELLOWNOTE";
 		var nrOfNotes = 0;
-		var notes = [];
+		var notesData = [];
+		var renderNotes = [];
 
 		if(localStorage.getItem("SelectColor")) {
 			selectColor = localStorage.getItem("SelectColor")
@@ -52,14 +58,19 @@ class Home extends React.Component {
 		if(localStorage.getItem("NrOfNotes")) {
 			nrOfNotes = localStorage.getItem("NrOfNotes")
 		}
-		if(localStorage.getItem("Notes")) {
-		//	notes = JSON.parse(localStorage.getItem("Notes") || "[]").notes;
+		if(localStorage.getItem("NotesData")) {
+			notesData = JSON.parse(localStorage.getItem("NotesData") || "[]").notesData;
+		}
+		if(localStorage.getItem("RenderNotes")) {
+			renderNotes = JSON.parse(localStorage.getItem("RenderNotes") || "[]").renderNotes;
 		}
 
 		return {
 			selectedColor: selectColor,
-			nrOfNotes: nrOfNotes,
-			notes: notes
+			nrOfNotes: parseInt(nrOfNotes, 10),
+			notesData: notesData,
+			renderNotes: renderNotes,
+			isReloaded: true
 		};
 	}
 
@@ -67,28 +78,75 @@ class Home extends React.Component {
 		this.state.selectedColor = event.target.value;
 	}
 
-	onPageUnload() {
-		localStorage.clear();
+	onHashChange(event) {
+		this.state.selectedColor = window.location.hash.split('#')[1];
+	}
+
+	onPageUnload(event) {
+		for (var i = 0; i < this.state.nrOfNotes; i++) {
+
+			console.log(localStorage.getItem(this.state.notesData[i].noteHeaderId));
+			this.state.notesData[i].headerContent = localStorage.getItem(this.state.notesData[i].noteHeaderId);
+			this.state.notesData[i].bodyContent = localStorage.getItem(this.state.notesData[i].noteBodyId);
+		}
 
 		localStorage.setItem("NrOfNotes", this.state.nrOfNotes);
 		localStorage.setItem("SelectColor", this.state.selectedColor);
-		localStorage.setItem("Notes", JSON.stringify({notes: this.state.notes}));
+		localStorage.setItem("NotesData", JSON.stringify({notesData: this.state.notesData}));
+		localStorage.setItem("RenderNotes", JSON.stringify({renderNotes: this.state.renderNotes}));
+
+		// Chrome requires returnValue to be set
+  		event.returnValue = '';
 	}
 
 	render() {
-		var renderNotes = []
-		if (this.state.nrOfNotes != 0) {
-			for (var i = (this.state.nrOfNotes - 1); i < this.state.nrOfNotes; i += 1) {
-				this.state.notes.push({
-					id: i,
-					color: this.state.selectedColor
-				});
-				renderNotes.push(<StickyNote color={this.state.selectedColor} />);
+		var noOfNotesToRender = 0;
+		var noPush = true;
+		if(this.state.isReloaded) {
+			noOfNotesToRender = 0;		// Render all notes.
+			this.state.isReloaded = false;
+
+			for (var i = noOfNotesToRender; i < this.state.nrOfNotes; i += 1) {
+
+				this.state.renderNotes.push(
+					<StickyNote
+						color={this.state.notesData[i].color}
+						headerContent={this.state.notesData[i].headerContent}
+						bodyContent={this.state.notesData[i].bodyContent}
+						id={i}
+					/>
+				);
 			};
+		} else {
+			noOfNotesToRender = (this.state.nrOfNotes - 1); // Render only the last note added.
+			if (this.state.nrOfNotes != 0) {
+				for (var i = noOfNotesToRender; i < this.state.nrOfNotes; i += 1) {
+					this.state.notesData.push({
+						key: "sticky-note-" + i,
+						noteHeaderId: "noteHeader" + i,
+						noteBodyId: "noteBody" + i,
+						headerContent: "Title",
+						bodyContent: "Content",
+						color: this.state.selectedColor
+					});
+					this.state.renderNotes.push(
+						<StickyNote
+							color={this.state.selectedColor}
+							headerContent={this.state.notesData[i].headerContent}
+							bodyContent={this.state.notesData[i].bodyContent}
+							id={i}
+						/>
+					);
+				};
+			}
 		}
+
 		return (
-			<div>
+			<div className={classes.Home}>
 				<div className={classes.HeaderContainer}>
+					<div className={classes.Wrapper}>
+						<h1 className={classes.Header}>Stickiess app</h1>
+					</div>
 					<div className={classes.Wrapper}>
 						<SVG src={file}  width={`2rem`} />
 						<p className={classes.NonClickHeading}>{this.state.nrOfNotes}</p>
@@ -104,17 +162,21 @@ class Home extends React.Component {
 
 					<div className={classes.Wrapper} onClick={this.onChangeColorSelection} >
 						<SVG src={draw} width={`2rem`}  />
-						<p>Color</p>
-						<select className={classes.ColorOption}>
-							<option value="YELLOWNOTE">Yellow</option>
-							<option value="BLACKNOTE">Black</option>
-							<option value="BLUENOTE">Blue</option>
-						</select>
+						<p className={classes.Heading}>Color</p>
+						<div className={classes.DropdownColor}>
+
+						    <SVG className={classes.DropdownArrow} src={arrow} width={`1rem`}  />
+							<div className={classes.DropdownColorContent}>
+						      <a className={classes.YellowBackGround} href="#YELLOWNOTE">Yellow</a>
+						      <a className={classes.BlueBackGround} href="#BLUENOTE">Blue</a>
+						      <a className={classes.BlackBackGround} href="#BLACKNOTE">Black</a>
+						    </div>
+						</div>
 					</div>
 
 				</div>
 				<div className={classes.HomeContainer}>
-					{renderNotes}
+					{this.state.renderNotes}
 				</div>
 			</div>
 		);
--- src/components/StickyNote/StickyNote.jsx
+++ src/components/StickyNote/StickyNote.jsx
@@ -9,7 +9,16 @@ import drag from "../../media/SVG/drag.svg";
 class StickyNote extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.saveValue = this.saveValue.bind(this);
 	}
+
+	saveValue(event) {
+		var id = event.target.id;  // get the sender's id to save it .
+        var val = event.target.value; // get the value.
+        localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override .
+	}
+
 	render() {
 		var colorStyles = "YellowNote";
 		if(this.props.color === "YELLOWNOTE"){
@@ -20,17 +29,16 @@ class StickyNote extends React.Component {
 			colorStyles = "" + classes.BlackNote;
 		}
 		return (
-			<div className={colorStyles} id={classes.defaultNoteStyle}>
+			<div className={colorStyles} id={classes.defaultNoteStyle} ref={`sticky-note-${this.props.id}`}>
 				<div className={classes.Sticky}>
 					<div className={classes.Header}>
 						<SVG src={block} />
-						<label for="title" />
-						<input type="text"  name="title" placeholder="title" className={classes.title} />
+						<label htmlFor="title" />
+						<input  id={`noteHeader${this.props.id}`} onKeyUp={this.saveValue} type="text" defaultValue={this.props.headerContent} name="title" className={classes.Title}/>
 						<SVG src={drag} />
 					</div>
 					<div className="NoteArea">
-						<textarea className={classes.content} rows="4" cols="50">
-							hellow
+						<textarea id={`noteBody${this.props.id}`} onKeyUp={this.saveValue} className={classes.Content} rows="4" cols="50" ref="stNoteBody" defaultValue={this.props.bodyContent}>
 						</textarea>
 					</div>
 				</div>
